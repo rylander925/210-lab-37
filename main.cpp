616e893eca93ed500312bd44cc9d4827250f8a24
rylander925 <rylander925@gmail.com>
2025-12-05 18:46:37 -0800

    added basic framework stuff

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/*
+COMSC-210 | Lab 37 | Rylan Der
+IDE Used: Visual Studio Code
+*/
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include <fstream>
+using namespace std;
+
+int sum_ascii(string);
+
+int main() {
+    char a = 'A';
+    cout << a << endl;
+    cout << (int) a << endl;
+    int b = 66;
+    cout << b << endl;
+    cout << (char) b << endl;
+    
+
+    return 0;
+}
+
+/* 
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
+
+/**
+ * Sums ascii values of the characters of the given string
+ * @param str String whose ascii values will be summed
+ * @return Sum of ascii values as an integer
+ */
+int sum_ascii(string str) {
+    
+}
